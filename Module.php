f7f8e0e1baa60a3b2adeb5401a87c7ad28332b30
anis <anis@safine.me>
2013-04-14 16:07:06 +0200

    Initial commit

---
+++ Module.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CRUD actions module for Doctrine entities on Zend Framework 2
+ *
+ * PHP Version 5.4.13
+ *
+ * @author  Anis Safine <anis@safine.me>
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace DoctrineCRUD;
+
+/**
+ * CRUD actions module for Doctrine entities on Zend Framework 2
+ *
+ * @author  Anis Safine <anis@safine.me>
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+class Module
+{
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+CRUD actions generator for Doctrine 2 entities on Zend Framework 2
+=================
+
+License
+-------
+
+This library is licensed under the New BSD License - see the LICENSE file for details.
